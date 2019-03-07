a4f25df49afcb5d8d42fc64fef2a766ac28a6219
sm00th <ezzybrzy@sm00ths-MacBook-Air.local>
2019-03-07 14:49:55 -0600

    done

---
+++ problem2.rb
@@ -0,0 +1,14 @@
+
+array = [1,2,3,4,5,6,7,8,9]
+def two_sum(numbers, target)  
+  for x in numbers.size
+    x2 = numbers[x+1] 
+    while x < x2
+      if total = x2 + x1
+        puts "got it" 
+      end 
+    end
+    x+=1
+  end
+end
+two_sum(array, 5)
